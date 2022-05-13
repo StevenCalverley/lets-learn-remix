f4e08803446be41e1ec65dbc1e4b79616c518e99
Steve Calverley <steve.calverley@heffron.com.au>
2022-05-13 17:01:08 +1000

    Add Prisma and Init work on Users

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,27 @@
+import type { User } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { users: Array<User> };
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    users: await db.user.findMany(),
+  };
+  return json(data);
+};
+
 export default function Index() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
       <h1 className="text-lg">Welcome to Remix</h1>
+      <ul>
+        {data.users.map((user) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
     </div>
   );
 }
